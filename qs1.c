bd153213c8854b7fcad99b5b41fcbbdd155b3182
deepanshu <deepanshuarya0911@gmail.com>
2026-08-15 14:58:10 +0530

    Add files via upload

---
+++ qs1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+	int a=3;
+	int b=4;
+	int sum1=a+b;
+	printf("the addition1 of a and b is %d\n", sum1);
+
+ 	int c=-1;
+	int d=20;
+	int sum2= c+d;
+	printf("the addition2 of c and d is %d\n", sum2);
+
+	return 0;
+}
