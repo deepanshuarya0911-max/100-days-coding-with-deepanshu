36b4dc00eb0f7970a18ef2752dcfcb7d3cf763a4
deepanshu <deepanshuarya0911@gmail.com>
2026-08-20 17:45:55 +0530

    Add files via upload

---
+++ qs22.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    float cp, sp, percentage;
+
+    scanf("%f %f", &cp, &sp);
+
+    if (sp>cp) {
+        percentage = ((sp-cp) / cp) * 100;
+        printf("profit %.0f%%", percentage);
+    }
+
+    else if (cp>sp) {
+        percentage = ((cp-sp) / cp) * 100;
+        printf("loss %.0f%%", percentage);
+    }
+
+    else {
+        printf("no profit no loss");
+    }
+
+    return 0;
+}
