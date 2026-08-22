09befeacdc55e7752d0b5d84f6659cfa82c3552e
deepanshu <deepanshuarya0911@gmail.com>
2026-08-22 20:08:11 +0530

    Add files via upload

---
+++ qs25.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    switch(op) {
+        case '+':
+        printf("%d", a+b);
+        break;
+
+        case '-':
+        printf("%d", a-b);
+        break;
+
+        case '*':
+        printf("%d", a*b);
+        break;
+
+        case '/':
+        if(b !=0)
+        printf("%d", a/b);
+
+        else
+        printf("cannot divide by zero");
+        break;
+
+        default:
+        printf("invalid operator");
+    }
+
+    return 0;
+}
